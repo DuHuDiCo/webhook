f30e6235d4f24a23d73f3952ad5237c1f1392cf9
DuHuDiCo <dudicodiaz@gmail.com>
2025-01-08 11:27:10 -0500

    commti

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-
+from fastapi import FastAPI, File, UploadFile, HTTPException,Request
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -10,3 +10,15 @@ async def webhook(file: UploadFile = File(...)):
         return {"message": "Archivo procesado correctamente"}
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
+    
+    
+@app.get("/webhook")
+async def verify_webhook(request: Request, hub_mode: str = None, hub_challenge: str = None, hub_verify_token: str = None):
+    # Aquí validas el `hub_verify_token` con el valor esperado
+    expected_token = "EAAXRz1H2U84BOzvhWOslZAnVryCduZCQb8XWmk5tZAcxuOJKOiEwtH83maCUz8MMMZAnuDe4Y0y3kkdDoAycjl5SLWRVO5EvYKroSSDdziC0BbtSTDBt81FlQLGvWT6eMm0xGJZBAXHZAIKnBrOuvwnr4km0ekOuzTOOJxVlJZBICv6ygut1tRHz3rlnmvsQgBDKRjzn0ZCMzpsdqKlCVjy0ePko7IW2won7gOt4PditCAZDZD"
+    
+    if hub_verify_token == expected_token:
+        if hub_mode == "subscribe":
+            return PlainTextResponse(content=hub_challenge)
+    else:
+        return PlainTextResponse(content="Unauthorized", status_code=403)
