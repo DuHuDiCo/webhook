6aecd72535afcb06032fd5d8e6ef167a4d451ceb
DuHuDiCo <dudicodiaz@gmail.com>
2025-01-08 10:42:00 -0500

    commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 @app.post("/webhook")
 async def webhook(file: UploadFile = File(...)):
     try:
+        print({"filename": file.filename, "content_type": file.content_type})
         return {"message": "Archivo procesado correctamente"}
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
