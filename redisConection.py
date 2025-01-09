7e3054baf27399668be4e4fe80de7510d77099c9
DuHuDiCo <dudicodiaz@gmail.com>
2025-01-09 15:14:33 -0500

    commit

--- redisConection.py
+++ redisConection.py
@@ -1,11 +1,13 @@
-import redis
+import redis, json
 
 # Configuración de Redis
 r = redis.Redis(host='192.168.1.241', port=6379, decode_responses=True)
 
 def guardar_datos_en_redis(telefono, campo, valor):
     clave = f"session:{telefono}"
-    r.hset(clave, campo, valor)
+    dictionary = {campo: valor}
+    json_data = json.dumps(dictionary)
+    r.rpush(clave, json_data)
     r.expire(clave, 3600)  # Expira en 1 hora
 
 
