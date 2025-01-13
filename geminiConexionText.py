003e9ef5c8bb08b54c41150910c9603cc28c0c1e
DuHuDiCo <dudicodiaz@gmail.com>
2025-01-13 12:34:17 -0500

    commit

--- geminiConexionText.py
+++ geminiConexionText.py
@@ -5,8 +5,8 @@ genai.configure(api_key="AIzaSyATWEAxTXNCLxJ2hjy8Dcoi_Ovsy7ALmkQ")
 instruction = """Recibirás palabras o textos como entrada. Tu tarea es verificar si la entrada coincide exactamente con el nombre de algún banco que opera en Colombia. Si hay coincidencia, responde únicamente con ese nombre exacto del banco. Si no hay coincidencia, no respondas nada (respuesta vacía).
 
 Reglas:
-1. Solo responde con el nombre exacto del banco si hay coincidencia.
-2. Devuelve un false si no hay coincidencia o un true si hay coincidencia.
+1. Solo responde true si hay coincidencia.
+2. Devuelve un false si no hay coincidencia .
 3. No incluyas explicaciones ni texto adicional.
 4. La comparación debe ignorar mayúsculas y minúsculas.
 5. Considera tanto nombres comunes como nombres oficiales de los bancos.
